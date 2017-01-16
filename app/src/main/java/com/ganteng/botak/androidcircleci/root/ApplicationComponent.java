24899037b167bdb59378fedbafaf049fa65605d5
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-16 14:17:08 +0700

    update module

--- app/src/main/java/com/ganteng/botak/androidcircleci/root/ApplicationComponent.java
+++ app/src/main/java/com/ganteng/botak/androidcircleci/root/ApplicationComponent.java
@@ -4,11 +4,14 @@ import com.ganteng.botak.androidcircleci.login.LoginActivity;
 
 import javax.inject.Singleton;
 
+import dagger.Component;
+
 /**
  * Created by adityahadiwijaya on 1/16/17.
  */
 
 @Singleton
+@Component(modules = {ApplicationModule.class})
 public interface ApplicationComponent {
 
     void inject(LoginActivity target);
