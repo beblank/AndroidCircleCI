b5c87da833ee0674189547fcee45c0c614cbd149
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-16 16:16:08 +0700

    update Android Manifest name to root.App

--- app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
@@ -2,14 +2,35 @@ package com.ganteng.botak.androidcircleci.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.ganteng.botak.androidcircleci.R;
+import com.ganteng.botak.androidcircleci.root.App;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.button_submit)
+    Button submitButton;
+
+    @BindView(R.id.input_username)
+    EditText usernameInput;
+
+    @BindView(R.id.input_password)
+    EditText passwordInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ((App)getApplication()).getComponent().inject(this);
+
+        ButterKnife.bind(this);
+
     }
 }
