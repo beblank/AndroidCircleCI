0ba50b6bfd3b54b637b8d8ef99c1f33e04ab7b3a
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-16 14:00:01 +0700

    create root app

--- app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
@@ -1,7 +1,9 @@
-package com.ganteng.botak.androidcircleci;
+package com.ganteng.botak.androidcircleci.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.ganteng.botak.androidcircleci.R;
 
 public class LoginActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/ganteng/botak/androidcircleci/root/App.java
@@ -0,0 +1,8 @@
+package com.ganteng.botak.androidcircleci.root;
+
+/**
+ * Created by adityahadiwijaya on 1/16/17.
+ */
+
+public class App {
+}
