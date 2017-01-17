f5b19a64452cab207a7d6aa27272c635f1395714
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-17 17:13:05 +0700

    create User model

---
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivityMVP.java
@@ -0,0 +1,8 @@
+package com.ganteng.botak.androidcircleci.login;
+
+/**
+ * Created by adityahadiwijaya on 1/17/17.
+ */
+
+public interface LoginActivityMVP {
+}
---
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/model/User.java
@@ -0,0 +1,41 @@
+package com.ganteng.botak.androidcircleci.login.model;
+
+/**
+ * Created by adityahadiwijaya on 1/17/17.
+ */
+
+public class User {
+
+    private int id;
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
