9ec9455a1177d41bd73256fadecc1b308abceb8c
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-17 17:51:48 +0700

    update login Activity

--- app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginActivity.java
@@ -2,16 +2,22 @@ package com.ganteng.botak.androidcircleci.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.ganteng.botak.androidcircleci.R;
 import com.ganteng.botak.androidcircleci.root.App;
 
+import javax.inject.Inject;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements LoginActivityMVP.View {
+
+    @Inject
+    LoginActivityMVP.Presenter presenter;
 
     @BindView(R.id.button_submit)
     Button submitButton;
@@ -31,6 +37,48 @@ public class LoginActivity extends AppCompatActivity {
         ((App)getApplication()).getComponent().inject(this);
 
         ButterKnife.bind(this);
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                presenter.loginButtonClicked();
+            }
+        });
+
+    }
+
+    @Override
+    public String getUserName() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public void showUserNotAvailable() {
+
+    }
+
+    @Override
+    public void showInputError() {
+
+    }
+
+    @Override
+    public void showUserSavedMessage() {
+
+    }
+
+    @Override
+    public void setUsername(String uname) {
+
+    }
+
+    @Override
+    public void setPassword(String pass) {
 
     }
 }
