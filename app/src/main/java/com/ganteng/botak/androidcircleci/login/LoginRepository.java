f21f0d5d96b051924278e3eff62218c190588f2e
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-17 17:32:26 +0700

    create login repository

---
+++ app/src/main/java/com/ganteng/botak/androidcircleci/login/LoginRepository.java
@@ -0,0 +1,12 @@
+package com.ganteng.botak.androidcircleci.login;
+
+/**
+ * Created by adityahadiwijaya on 1/17/17.
+ */
+
+public interface LoginRepository {
+
+    User getUser();
+    
+    void saveUser(User user);
+}
