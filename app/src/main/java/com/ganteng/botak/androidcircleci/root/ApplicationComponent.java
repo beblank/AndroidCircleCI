5b333caa3a6e24ea6ebd955c49acbe0e974f7dc5
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-17 17:52:26 +0700

    update Application Component with login module

--- app/src/main/java/com/ganteng/botak/androidcircleci/root/ApplicationComponent.java
+++ app/src/main/java/com/ganteng/botak/androidcircleci/root/ApplicationComponent.java
@@ -1,6 +1,8 @@
 package com.ganteng.botak.androidcircleci.root;
 
 import com.ganteng.botak.androidcircleci.login.LoginActivity;
+import com.ganteng.botak.androidcircleci.login.LoginModel;
+import com.ganteng.botak.androidcircleci.login.LoginModule;
 
 import javax.inject.Singleton;
 
@@ -11,7 +13,7 @@ import dagger.Component;
  */
 
 @Singleton
-@Component(modules = {ApplicationModule.class})
+@Component(modules = {ApplicationModule.class, LoginModule.class})
 public interface ApplicationComponent {
 
     void inject(LoginActivity target);
