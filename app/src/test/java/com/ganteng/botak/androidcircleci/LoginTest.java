60ab2d7c42eaf8e5bb0ccd46262d31b2bb7c2b1d
Aditya Hadiwijaya <kulik2@ymail.com>
2017-01-18 16:45:19 +0700

    testing for failed unit test

--- app/src/test/java/com/ganteng/botak/androidcircleci/LoginTest.java
+++ app/src/test/java/com/ganteng/botak/androidcircleci/LoginTest.java
@@ -1,8 +1,39 @@
 package com.ganteng.botak.androidcircleci;
 
+import com.ganteng.botak.androidcircleci.login.LoginActivityMVP;
+import com.ganteng.botak.androidcircleci.login.LoginActivityPresenter;
+import com.ganteng.botak.androidcircleci.login.User;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verifyZeroInteractions;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by adityahadiwijaya on 1/18/17.
  */
 
 public class LoginTest {
+    LoginActivityMVP.Model mockLoginModel;
+    LoginActivityMVP.View mockLoginView;
+    LoginActivityMVP.Presenter presenter;
+    User user;
+
+    @Before
+    public void setup(){
+        mockLoginModel = mock(LoginActivityMVP.Model.class);
+        mockLoginView = mock(LoginActivityMVP.View.class);
+        presenter = new LoginActivityPresenter(mockLoginModel);
+        presenter.setView(mockLoginView);
+    }
+
+    @Test
+    public void noInteractionWithView(){
+        presenter.getCurrentUser();
+        verifyZeroInteractions(mockLoginView);
+    }
+
+
 }
